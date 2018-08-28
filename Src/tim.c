6b1e7bcf05492b1ce660182a4b1d7ce10cab13c6
lutfisan <m_lutfi_santoso@yahoo.co.id>
2018-08-28 10:33:12 +0700

    change switching frecuency

--- Src/tim.c
+++ Src/tim.c
@@ -55,9 +55,9 @@ void MX_TIM1_Init(void)
   TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig;
 
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 800-1;
+  htim1.Init.Prescaler = 4-1;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 2000-1;
+  htim1.Init.Period = 1000-1;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
